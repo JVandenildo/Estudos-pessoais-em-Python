b721c21ef53ff1068232369fcc30c0db51dfa901
JVandenildo <jvandenildo@gmail.com>
2023-06-03 13:17:33 -0300

    web with python basics

---
+++ WebDev w. Python/website/__init__.py
@@ -0,0 +1,15 @@
+# this file turns the directory on a package
+# anything inside this file will execute automatically when imported
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET KEY'] = 'eyiwgtngia DFGig'
+    
+    from .views import views
+    from .auth import auth
+    
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    return app
