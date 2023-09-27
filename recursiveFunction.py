79e30a7925d7781c5e40bf467debe5baa7f6733e
J. Vandenildo <jvandenildo@gmail.com>
2023-09-27 16:27:44 -0300

    indirect recursive function (unfinished)

--- recursiveFunction.py
+++ recursiveFunction.py
@@ -1,3 +1,4 @@
+# direct recursive
 def factorial(n):
     if n == 1:
         return 1
@@ -7,3 +8,19 @@ def factorial(n):
 
 userNumber = int(input("Enter a number: "))
 print(factorial(userNumber))
+
+
+# indirect recursive
+def is_even(x):
+    if x % 2 == 0:
+        return True
+    else:
+        return is_odd(x - 1)
+
+
+def is_odd(x):
+    return is_even(x)
+
+
+print(is_odd(17))
+print(is_even(23))
